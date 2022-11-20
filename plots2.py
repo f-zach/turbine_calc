bfc55e86c93adf39ff0f9edc930c86bbe64cb96b
f-zach <63615390+f-zach@users.noreply.github.com>
2022-11-21 00:25:33 +0100

    added torque map

--- plots2.py
+++ plots2.py
@@ -54,12 +54,15 @@ xx = np.append(xx, x, 0)
 yy = np.append(yy, y, 0)
 zz = np.append(zz, (y*1e3)/(z[:, 2]*43.1e3*300/350)*100, 0)
 
+print(y)
 print(np.amax(x), np.amax(y))
 
 
 ax.plot(x, y,'k')
 ax.plot(np.amax(x), np.amax(y), '.r', markersize=10)
 ax.text(np.amin(x)-340,np.amin(y)-0.15,'100\\%',fontsize = 14)
+
+print(np.amax(x),np.amax(y),np.amin(z[:,10]))
 
 
 z = getMeanData(directory, 95)
@@ -144,7 +147,7 @@ yc = np.append(yc,np.amin(y))
 xx = np.append(xx, x, 0)
 yy = np.append(yy, y, 0)
 zz = np.append(zz, (y*1e3)/(z[:, 2]*43.1e3*300/350)*100, 0)
-
+print(zz)
 ax.plot(x, y,'k')
 ax.text(np.amin(x)-270,np.amin(y)-0.15,'50\\%',fontsize = 14)
 
@@ -216,7 +219,9 @@ CS = ax.contour(x_grid, y_grid, grid1, 20,
 ax.clabel(CS, inline=1,inline_spacing = -7, fontsize=14,fmt="%1.1f")
 ax.text(x_grid[Xmax, Ymax]+30, y_grid[Xmax, Ymax] +
          0.2, "%.2f" % np.nanmax(grid1),fontsize=14)
-ax.set_xlim((1500,7000))
+
+ax.grid(1,'both',ls='--')
+ax.set_xlim((1600,7000))
 ax.set_ylim((0,13.2))
 plt.xlabel('n\\textsubscript{2} [RPM]')
 plt.ylabel('P [kW]')
@@ -236,24 +241,23 @@ zz = []
 xc = []
 yc = []
 
-for thr in range(90,101,10):
-    z = getMeanData(directory, thr)
+z = getMeanData(directory, 100)
 
-    x = z[:, 9]
+x = z[:, 9]
 
-    y = z[:, 10]
+y = z[:, 10]
 
-    xc = np.append(xc,np.amin(x))
-    yc = np.append(yc,np.amin(y))
+xc = np.append(xc,np.amin(x))
+yc = np.append(yc,np.amax(y))
 
-    xx = np.append(xx, x, 0)
-    yy = np.append(yy, y, 0)
-    zz = np.append(zz, (y*1e3)/(z[:, 2]*43.1e3*300/350)*100, 0)
+xx = np.append(xx, x, 0)
+yy = np.append(yy, y, 0)
+zz = np.append(zz, (z[:,11]*1e3)/(z[:, 2]*43.1e3*300/350)*100, 0)
 
-    ax.plot(x, y,'k')
+ax.plot(x, y,'k')
+ax.text(np.amin(x)-330,np.amax(y)-0.15,'100\\%',fontsize = 14)
 
 
-#95%
 z = getMeanData(directory, 95)
 
 x = z[:, 9]
@@ -261,13 +265,37 @@ x = z[:, 9]
 y = z[:, 10]
 
 xc = np.append(xc,np.amin(x))
-yc = np.append(yc,np.amin(y))
+yc = np.append(yc,np.amax(y))
 
 xx = np.append(xx, x, 0)
 yy = np.append(yy, y, 0)
-zz = np.append(zz, (y*1e3)/(z[:, 2]*43.1e3*300/350)*100, 0)
+zz = np.append(zz, (z[:,11]*1e3)/(z[:, 2]*43.1e3*300/350)*100, 0)
 
 ax.plot(x, y,'k')
+ax.text(np.amin(x)-270,np.amax(y)-0.15,'95\\%',fontsize = 14)
+
+    
+
+
+#95%
+z = getMeanData(directory, 90)
+
+x = z[:, 9]
+
+y = z[:, 10]
+
+xc = np.append(xc,np.amin(x))
+yc = np.append(yc,np.amax(y))
+
+xx = np.append(xx, x, 0)
+yy = np.append(yy, y, 0)
+zz = np.append(zz, (z[:,11]*1e3)/(z[:, 2]*43.1e3*300/350)*100, 0)
+
+
+ax.plot(x, y,'k')
+ax.text(np.amin(x)-270,np.amax(y)-0.15,'90\\%',fontsize = 14)
+
+
 
 #85%
 z = getMeanData(directory, 85)
@@ -277,13 +305,16 @@ x = z[:, 9]
 y = z[:, 10]
 
 xc = np.append(xc,np.amin(x))
-yc = np.append(yc,np.amin(y))
+yc = np.append(yc,np.amax(y))
 
 xx = np.append(xx, x, 0)
 yy = np.append(yy, y, 0)
-zz = np.append(zz, (y*1e3)/(z[:, 2]*43.1e3*300/350)*100, 0)
+zz = np.append(zz, (z[:,11]*1e3)/(z[:, 2]*43.1e3*300/350)*100, 0)
 
 ax.plot(x, y,'k')
+ax.text(np.amin(x)-270,np.amax(y)-0.15,'85\\%',fontsize = 14)
+
+
 
 #75%
 z = getMeanData(directory, 75)
@@ -292,11 +323,17 @@ x = z[:, 9]
 
 y = z[:, 10]
 
-ax.plot(x, y,'k')
-
-
 xc = np.append(xc,np.amin(x))
-yc = np.append(yc,np.amin(y))
+yc = np.append(yc,np.amax(y))
+
+xx = np.append(xx, x, 0)
+yy = np.append(yy, y, 0)
+zz = np.append(zz, (z[:,11]*1e3)/(z[:, 2]*43.1e3*300/350)*100, 0)
+
+ax.plot(x, y,'k')
+ax.text(np.amin(x)-270,np.amax(y)-0.15,'75\\%',fontsize = 14)
+
+
 
 #50%
 z = getMeanData(directory, 50)
@@ -305,22 +342,88 @@ x = z[:, 9]
 
 y = z[:, 10]
 
-
 xc = np.append(xc,np.amin(x))
-yc = np.append(yc,np.amin(y))
+yc = np.append(yc,np.amax(y))
 
 xx = np.append(xx, x, 0)
 yy = np.append(yy, y, 0)
-zz = np.append(zz, (y*1e3)/(z[:, 2]*43.1e3*300/350)*100, 0)
-
+zz = np.append(zz, (z[:,11]*1e3)/(z[:, 2]*43.1e3*300/350)*100, 0)
 
 ax.plot(x, y,'k')
+ax.text(np.amin(x)-270,np.amax(y)-0.15,'50\\%',fontsize = 14)
 
-ax.set_xlim((1500,7000))
+points = [xx, yy]
+points = np.transpose(points)
+values = zz
+
+griddim = 100j
+x_grid, y_grid = np.mgrid[1800:7200:griddim, 2:24:griddim]
+
+grid1 = interp.griddata(points, values, (x_grid, y_grid), method='cubic',rescale=1)
+print(xc,yc)
+i = 0
+while i < grid1.shape[0]:
+    j = 0
+    while j < grid1.shape[1]:
+    
+        if (x_grid[i,j] <= xc[4] or x_grid[i,j] <= xc[5]) and (y_grid[i,j] >= yc[5] or y_grid[i,j] <= yc[4]) and not math.isnan(grid1[i,j]):
+            v1 = (xc[4]-xc[5],yc[4]-yc[5])
+            vc = (x_grid[i,j]-xc[5],y_grid[i,j]-yc[5])
+
+            cp = v1[0]*vc[1] - v1[1]*vc[0]
+            if cp > 0:
+                grid1[i,j] = float('nan')
+        
+        if (x_grid[i,j] <= xc[3] or x_grid[i,j] <= xc[4]) and (y_grid[i,j] >= yc[4] and y_grid[i,j] <= yc[3]) and not math.isnan(grid1[i,j]):
+            v1 = (xc[3]-xc[4],yc[3]-yc[4])
+            vc = (x_grid[i,j]-xc[4],y_grid[i,j]-yc[4])
+
+            cp = v1[0]*vc[1] - v1[1]*vc[0]
+            if cp > 0:
+                grid1[i,j] = float('nan')
+
+        if (x_grid[i,j] <= xc[3] or x_grid[i,j] <= xc[0]) and (y_grid[i,j] >= yc[3] and y_grid[i,j] <= yc[0]) and not math.isnan(grid1[i,j]):
+            v1 = (xc[0]-xc[3],yc[0]-yc[3])
+            vc = (x_grid[i,j]-xc[3],y_grid[i,j]-yc[3])
+
+            cp = v1[0]*vc[1] - v1[1]*vc[0]
+            if cp > 0:
+                grid1[i,j] = float('nan')
+
+        if (x_grid[i,j] <= xc[0] or x_grid[i,j] <= xc[1]) and (y_grid[i,j] >= yc[0] and y_grid[i,j] <= yc[1]) and not math.isnan(grid1[i,j]):
+            v1 = (xc[1]-xc[0],yc[1]-yc[0])
+            vc = (x_grid[i,j]-xc[0],y_grid[i,j]-yc[0])
+
+            cp = v1[0]*vc[1] - v1[1]*vc[0]
+            if cp > 0:
+                grid1[i,j] = float('nan')
+
+
+
+        j += 1
+
+
+    i += 1
+    f.printProgressBar(i,np.imag(griddim),'Masking:',)
+    
+Xmax, Ymax = np.unravel_index(np.nanargmax(grid1), np.shape(grid1))
+
+CS = ax.contour(x_grid, y_grid, grid1, 20,
+                 linestyles='dashed', linewidths=1, cmap='Wistia')
+
+#ax.clabel(CS, CS.levels, inline=True, fontsize=10)
+
+
+#ax.plot(x_grid[Xmax, Ymax], y_grid[Xmax, Ymax], 'xr', markersize=5)
+ax.clabel(CS, inline=1,inline_spacing = -7, fontsize=14,fmt="%1.1f")
+ax.grid(1,'both',ls='--')
+ax.set_xlim((1600,7000))
 ax.set_ylim((0,25))
 
-plt.xlabel('n\\textsubscript{2}')
-plt.ylabel('Q')
+plt.xlabel('n\\textsubscript{2} [RPM]')
+plt.ylabel('M [Nm]')
 
-#plt.show()
+tikz.save("D:/Users/Fabian Zach/OneDrive/Uni/Masterarbeit/Latex/Arbeit/Ressourcen/Bilder/5/kennfeld_M_2.tex",axis_height = '\\figH', axis_width = '\\figW')
+
+plt.show()
 
